a826702366ebdbd276291bccc498d592b80ab443
zwados <lukasz.rozwadowski7@gmail.com>
2020-03-07 21:52:43 +0100

    Updates

---
+++ precache-manifest.530f999c0bf120660e54ded5e5a37b43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96c4648f55904a78c6884afc72961a3",
+    "url": "/message-builder/index.html"
+  },
+  {
+    "revision": "5430e6c899fc2c736df1",
+    "url": "/message-builder/static/css/main.a688f37e.chunk.css"
+  },
+  {
+    "revision": "e78895ad456be3db0bc8",
+    "url": "/message-builder/static/js/2.5af9edfd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/message-builder/static/js/2.5af9edfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5430e6c899fc2c736df1",
+    "url": "/message-builder/static/js/main.5c81cf32.chunk.js"
+  },
+  {
+    "revision": "4ff7a7b7d58286ca7292",
+    "url": "/message-builder/static/js/runtime-main.59f51bd8.js"
+  },
+  {
+    "revision": "4091f3510faa0583864e0ffbfd7aa47a",
+    "url": "/message-builder/static/media/bg.4091f351.jpg"
+  },
+  {
+    "revision": "9934596d88a8f3522909f520de4031f7",
+    "url": "/message-builder/static/media/iqvia.9934596d.jpg"
+  }
+]);
